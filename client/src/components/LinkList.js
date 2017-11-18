dd5ef17af5d76e8a6aec22a0c25e573cf7d80607
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-11-18 14:43:52 +0100

    render link list

---
+++ client/src/components/LinkList.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+
+import Link from './Link';
+
+// define query -> get all Links
+
+const ALL_LINKS_QUERY = gql`
+  # 2
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+    }
+  }
+`;
+
+const LinkList = ({ allLinksQuery }) => {
+  // data still loading
+  if (allLinksQuery && allLinksQuery.loading) {
+    return <div>Loading</div>;
+  }
+
+  // error occured
+  if (allLinksQuery && allLinksQuery.error) {
+    return <div>Error</div>;
+  }
+
+  // render links
+  const linksToRender = allLinksQuery.allLinks;
+
+  return (
+    <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
+  );
+};
+
+// export wrapped in graphql container
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' })(LinkList);
