6556e3a12b6447a8282806ee6cfe720ea39b1455
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-11-25 14:46:29 +0100

    add subscriptions

--- client/src/components/LinkList.js
+++ client/src/components/LinkList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
@@ -35,33 +35,125 @@ export const ALL_LINKS_QUERY = gql`
   }
 `;
 
-const LinkList = ({ allLinksQuery }) => {
-  // data still loading
-  if (allLinksQuery && allLinksQuery.loading) {
-    return <div>Loading</div>;
+// const LinkList = ({ allLinksQuery }) => {
+class LinkList extends Component {
+  componentDidMount() {
+    this.subscribeToNewLinks();
+    this.subscribeToNewVotes();
   }
 
-  // error occured
-  if (allLinksQuery && allLinksQuery.error) {
-    return <div>Error</div>;
+  subscribeToNewLinks = () => {
+    this.props.allLinksQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          Link(filter: { mutation_in: [CREATED] }) {
+            node {
+              id
+              url
+              description
+              createdAt
+              postedBy {
+                id
+                name
+              }
+              votes {
+                id
+                user {
+                  id
+                }
+              }
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const newAllLinks = [
+          ...previous.allLinks,
+          subscriptionData.data.Link.node,
+        ];
+        const result = {
+          ...previous,
+          allLinks: newAllLinks,
+        };
+        return result;
+      },
+    });
+  };
+
+  subscribeToNewVotes = () => {
+    this.props.allLinksQuery.subscribeToMore({
+      document: gql`
+        subscription {
+          Vote(filter: { mutation_in: [CREATED] }) {
+            node {
+              id
+              link {
+                id
+                url
+                description
+                createdAt
+                postedBy {
+                  id
+                  name
+                }
+                votes {
+                  id
+                  user {
+                    id
+                  }
+                }
+              }
+              user {
+                id
+              }
+            }
+          }
+        }
+      `,
+      updateQuery: (previous, { subscriptionData }) => {
+        const votedLinkIndex = previous.allLinks.findIndex(
+          link => link.id === subscriptionData.data.Vote.node.link.id,
+        );
+        const link = subscriptionData.data.Vote.node.link;
+        const newAllLinks = previous.allLinks.slice();
+        newAllLinks[votedLinkIndex] = link;
+        const result = {
+          ...previous,
+          allLinks: newAllLinks,
+        };
+        return result;
+      },
+    });
+  };
+
+  render() {
+    // data still loading
+    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+      return <div>Loading</div>;
+    }
+
+    // error occured
+    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+      return <div>Error</div>;
+    }
+
+    // render links
+    const linksToRender = this.props.allLinksQuery.allLinks;
+
+    return (
+      <div>
+        {linksToRender.map((link, index) => (
+          <Link
+            key={link.id}
+            updateStoreAfterVote={updateCacheAfterVote}
+            index={index}
+            link={link}
+          />
+        ))}
+      </div>
+    );
   }
-
-  // render links
-  const linksToRender = allLinksQuery.allLinks;
-
-  return (
-    <div>
-      {linksToRender.map((link, index) => (
-        <Link
-          key={link.id}
-          updateStoreAfterVote={updateCacheAfterVote}
-          index={index}
-          link={link}
-        />
-      ))}
-    </div>
-  );
-};
+}
 
 // export wrapped in graphql container
 export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' })(LinkList);
--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,9 @@ import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
-import { ApolloLink } from 'apollo-client-preset';
+import { ApolloLink, split } from 'apollo-client-preset';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
 import App from './components/App';
 import { GC_AUTH_TOKEN } from './constants';
@@ -32,9 +34,29 @@ const middlewareAuthLink = new ApolloLink((operation, forward) => {
 
 const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
 
+// subscription link
+const wsLink = new WebSocketLink({
+  uri: `wss://subscriptions.graph.cool/v1/cja5ax5200akm0171cqfoc8ah`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(GC_AUTH_TOKEN),
+    },
+  },
+});
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription';
+  },
+  wsLink,
+  httpLinkWithAuthToken,
+);
+
 // setup client
 const client = new ApolloClient({
-  link: httpLinkWithAuthToken,
+  link,
   cache: new InMemoryCache(),
 });
 
